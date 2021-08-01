f0a766f300910d8ab968bec34e8a4df7f53baa18
TakiKauser <takikauserbadhon@gmail.com>
2021-08-02 00:27:17 +0600

    while loop

---
+++ while-loop.js
@@ -0,0 +1,28 @@
+var itemsGiven = 0;
+
+while(itemsGiven < 9){
+    // console.log("Items available.");
+    itemsGiven++;
+    // console.log(itemsGiven);
+}
+
+// example
+var number = 1; //loop variable
+while(number <= 10){
+    // console.log(number);
+    number++; //counter increment
+}
+
+// odd numbers
+var number1 = 1; //loop variable
+while(number1 <= 20){
+    // console.log(number1);
+    number1 += 2; //counter increment
+}
+
+// even numbers
+var number2 = 0; //loop variable
+while(number2 <= 20){
+    console.log(number2);
+    number2 += 2; //counter increment
+}
