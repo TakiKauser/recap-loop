f7a80dd3e08c0a2b1170034419d791b97f7aea25
TakiKauser <takikauserbadhon@gmail.com>
2021-08-02 02:27:54 +0600

    array loop

---
+++ array-loop.js
@@ -0,0 +1,11 @@
+var numbers = [7, 3, 5, 9, 47, 49, 13, 97, 89]
+
+for(var i = 0; i < numbers.length; i++){
+    // console.log(numbers[i]);
+}
+
+var favorites = ["sai", "shosthika", "dhanush", "poriniti"];
+
+for(var i = 0; i < favorites.length; i++){
+    console.log(favorites[i]);
+}
